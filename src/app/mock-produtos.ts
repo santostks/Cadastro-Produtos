1c2b2ef657fe4593c75d5edb7a147acea53fd26a
santostks <bruno.santos244@fatec.sp.gov.br>
2022-11-08 08:50:02 -0300

    ProjetoCriado

---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,13 @@
+import { Produtos } from './produtos';
+
+
+export const PRODUTOS: Produtos[] = [
+  { id: 09, nome: 'ASUS Prime H310M-E', categoria: 'motherboard', quantidade: 5, origem: 'China' },
+  { id: 10, nome: 'GTX 1650 4GB', categoria: 'GPU', quantidade: 3, origem: 'Taiwan' },
+  { id: 11, nome: 'Fonte 150w Cougar', categoria: 'Fonte', quantidade: 8, origem: 'Japan' },
+  { id: 12, nome: 'SSD Kingston 120GB', categoria: 'SSD', quantidade: 4, origem: 'Taiwan' },
+  { id: 13, nome: 'RGB LED 30M', categoria: 'RGB', quantidade: 9, origem: 'China' },
+  { id: 14, nome: 'GTX 1080 4GB', categoria: 'GPU', quantidade: 3, origem: 'Taiwan' },
+  { id: 15, nome: 'GTX 2060 8GB', categoria: 'GPU', quantidade: 5, origem: 'Japan' },
+
+];
---
+++ src/app/produtos.ts
@@ -0,0 +1,7 @@
+export interface Produtos {
+  id: number;
+  nome: string;
+  categoria: string;
+  quantidade: number;
+  origem: string;
+}
